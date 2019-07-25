5831f8f1ad41d480beb02ca57c494c0fb10f2238
aze <aze@aze-mac.local>
2019-07-25 22:15:49 +0800

    draft home-page

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 $(function () {
-
+    $('.nav-mobile__opener').click(function(){
+        $('.nav-mobile').slideDown();
+        $('.nav-mobile__opener').css('display', 'none');
+      });
+      
+      $('.close__nav-mobile').click(function(){
+        $('.nav-mobile').slideUp();
+        $('.nav-mobile__opener').css('display', 'flex');
+      });
+      
+      $(document).click(function(event) {
+      if(!$(event.target).closest('.except-close').length) {
+        $('.nav-mobile').slideUp();
+        $('.nav-mobile__opener').css('display', 'flex');
+      } else {}});
+    
 });
