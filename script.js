6befba8b338e1a6e78bc284ee8a656ebd01c74e4
aze <aze@aze-mac.local>
2019-07-26 17:14:20 +0800

    complete home page

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 $(function () {
+
+          /////////////////////////
+  //// ヘッダ マウスオーバー時に色を変える
+  /////////////////////////
+
+      $('.nav__item-btn').hover(
+        function () {
+          $(this).find('.nav__item-page').css('color', '#FFB549');
+        },
+        function () {
+          $(this).find('.nav__item-page').css('color', '#000000');
+        }
+      );
+
+                /////////////////////////
+  //// ヘッダのハンバーガーメニューの開閉
+  /////////////////////////
+
+
     $('.nav-mobile__opener').click(function(){
         $('.nav-mobile').slideDown();
         $('.nav-mobile__opener').css('display', 'none');
@@ -14,5 +33,7 @@ $(function () {
         $('.nav-mobile').slideUp();
         $('.nav-mobile__opener').css('display', 'flex');
       } else {}});
+
+      
     
 });
